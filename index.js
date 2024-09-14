663bb57ea6a13e5b56d0c5f123d7108dcd814d41
UTOQINGAPP <utoqingapp@gmail.com>
2024-09-13 19:46:33 -0500

    update npm

--- index.js
+++ index.js
@@ -36,12 +36,6 @@ app.use('/api/messages',require('./routes/messages'));
 
 
 
-server.listen( process.env.PORT, ( err ) => {
 
-    if ( err ) throw new Error(err);
-
-    console.log('Servidor corriendo en puerto', process.env.PORT );
-
-});
 
 
