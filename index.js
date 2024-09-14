b874d05b8bed08dd1cfae0086222ebe06e066965
UTOQINGAPP <utoqingapp@gmail.com>
2024-09-13 20:04:27 -0500

    .env fix-1

--- index.js
+++ index.js
@@ -36,6 +36,12 @@ app.use('/api/messages',require('./routes/messages'));
 
 
 
+server.listen( process.env.PORT, ( err ) => {
 
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT );
+
+});
 
 
